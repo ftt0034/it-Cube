f5c14ba713afacc283cac77005b4ee692dbdba5d
ftt0034 <120639997+ftt0034@users.noreply.github.com>
2023-01-17 17:50:26 +0800

    Add files via upload

---
+++ January - 17/main.py
@@ -0,0 +1,16 @@
+a = float(input("Введите число А:"))
+b = float(input("Введите число В:"))
+c = float(input("Введите число С:"))
+d = float(input("Введите число D:"))
+f = float(input("Введите число F:"))
+
+sum_modulei = abs(a + b + c + d  + f)
+print(sum_modulei)
+
+
+
+
+
+
+
+
