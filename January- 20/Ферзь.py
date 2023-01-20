d96776036d3cce7f28b775cf09c463f4a9742017
ftt0034 <120639997+ftt0034@users.noreply.github.com>
2023-01-20 22:48:40 +0800

    Add files via upload

---
+++ January- 20/Ферзь.py
@@ -0,0 +1,10 @@
+a = int(input("Введите значение А:"))
+b = int(input("Введите значение В:"))
+a1= int(input("Введите значение А1:"))
+b1 = int(input("Введите значение В1:"))
+hod = a -b
+hod1 = a1 - b1
+if hod == hod1:
+    print("Yes")
+else:
+    print("No")
