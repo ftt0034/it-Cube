5262803337452ac5ec0152d54be021aea7d10eb0
ftt0034 <120639997+ftt0034@users.noreply.github.com>
2023-05-15 20:53:18 +0800

    Delete January - 19 directory

--- January - 19/Площадь,длина - круга.py
+++
@@ -1,5 +0,0 @@
-from math import pow,pi
-R = float(input("введите значение R:"))
-S = pi * pow(R, 2)
-C = 2*pi * R
-print( S, C)
--- January - 19/Синусы,Косинусы,Тангенсы.py
+++
@@ -1,7 +0,0 @@
-from math import sin,cos,tan,pow,radians
-x = float(input("Введите значение X: "))
-x = radians(x)
-x = sin(x) + cos(x) + pow(tan(x), 2)
-print (x)
-
-
